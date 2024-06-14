f39584af20f1c5e67945ffcb299130a7557d4d62
Baswakirank <patlollabaswakiran678@gmail.com>
2024-06-14 14:51:19 +0530

    first commit

---
+++ int.js
@@ -0,0 +1,18 @@
+let b=document.querySelector('#raina')
+b.addEventListener('click',show)
+function show()
+{
+    document.getElementById('demo').innerHTML='Suresh Raina[5] (pronunciationⓘ; born 27 November 1986[6]) is an Indian former international cricketer.[7] He occasionally serves as stand-in captain for Indian mens national cricket team as well as in IPL for Chennai Super Kings during the absence of the main captain, MS Dhoni. He played for Uttar Pradesh (UP) in domestic cricket circuit.[7] He is an aggressive left-handed middle-order batsman and occasional off-spin bowler. He is the second-youngest player ever to captain India.[8] He was the captain of Gujarat Lions in the Indian Premier League (IPL), and he also served as vice-captain of the Chennai Super Kings.[9] He is the first Indian batsman to hit a century in all three formats of international cricket.[10] During his time with India, he won the 2011 Cricket World Cup and the 2013 ICC Champions Trophy. Raina made history being the first Indian to score a century in the ICC Mens T20 World Cup, which he did with his 101 runs against South Africa at the 2010 Tournament On 15 August 2020, Raina announced his retirement from all formats of international cricket.[12] He pulled out of the 2020 Indian Premier League due to personal reasons.[13] On 6 September 2022, he announced his retirement from all forms of cricket, including IPL and domestic cricket.[14]'
+}
+let a=document.querySelector('#yours')
+a.addEventListener('click',disp)
+function disp()
+{
+    let h=prompt('enter ur fav car')
+    prompt('enter ur fav sport person')
+    prompt('enter ur fav dish')
+    prompt('enter ur fav place to visit')
+    prompt('enter ur fav actor')
+    prompt('enter ur fav politician')
+    alert('ur respose saved successfully u will receive a gift from us.....')
+}
